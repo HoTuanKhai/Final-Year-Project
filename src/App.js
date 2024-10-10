08535a2bf6098ed6283be343725e04a71a00badf
HoTuanKhai <khaihtgcd210244@fpt.edu.vn>
2024-10-10 20:58:12 +0700

    Modified app, index, Pages and Componenta

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
@@ -8,6 +7,9 @@ import FigureCategory from './Pages/FigureCategory';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import Footer from './Components/Footer/Footer';
+import leagueoflegend_banner from './Components/Asset/banner_leagueoflegend.jpg'
+import onepiece_banner from './Components/Asset/banner_onepiece.jpg'
+import pokemon_banner from './Components/Asset/banner_pokemon.jpg'
 
 function App() {
   return (
@@ -16,11 +18,11 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Figures/>}/>
-        <Route path='/leagueoflegend' element={<FigureCategory category="leagueoflegend"/>}/>
-        <Route path='/Onepiece' element={<FigureCategory category="onepiece"/>}/>
-        <Route path='/Pokemon' element={<FigureCategory category="pokemon"/>}/>
-        <Route path='/product' element={<Product/>}>
-          <Route path=':productId' element={<Product/>}/>
+        <Route path='/leagueoflegend' element={<FigureCategory banner={leagueoflegend_banner} category="leagueoflegend"/>}/>
+        <Route path='/onepiece' element={<FigureCategory banner={onepiece_banner} category="onepiece"/>}/>
+        <Route path='/Pokemon' element={<FigureCategory banner={pokemon_banner} category="pokemon"/>}/>
+        <Route path="/product" element={<Product />}>
+          <Route path=":productId" element={<Product />} />
         </Route>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignup/>}/>
